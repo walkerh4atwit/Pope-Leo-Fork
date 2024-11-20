6cf56e1514d81c854cebedc0a156fe53247019a2
rustoutlaw <rustoutlaw@gmail.com>
2024-11-20 14:58:48 +0000

    common

---
+++ common/defines/00_mb_defines.lua
@@ -0,0 +1,7 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 30
+NDefines.NReligion.MAX_RELIGIOUS_CENTER_SPREAD_DISTANCE = 200.0
+NDefines.NReligion.CONVERSION_ZEAL_DURATION = 2190
